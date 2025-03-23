13c74d389d67e282d27a714ae97d2b47fa304680
Aqeel <maqeel8080@gmail.com>
2025-03-23 16:12:02 +0500

    added google analytics to track visitors

---
+++ frontend/src/Analytics.js
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga4';
+
+function Analytics() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Track page views when the route changes
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+
+  return null;
+}
+
+export default Analytics;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
 import Router1 from './routes/Router1';
-import './style.css'
-
-
+import './style.css';
+import ReactGA from 'react-ga4';
+import { BrowserRouter } from 'react-router-dom';
+import Analytics from './Analytics';
 
 function App() {
+  // Initialize Google Analytics with your measurement ID
+  ReactGA.initialize('G-WZ6YB7NNHN');
+
   return (
-    <div className="App ">
-      <Router1/>
-    </div>
+    <BrowserRouter>
+      <Analytics /> {/* Tracks page views automatically */}
+      <div className="App">
+        <Router1 />
+      </div>
+    </BrowserRouter>
   );
 }
 
